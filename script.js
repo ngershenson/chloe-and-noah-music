dc2d5a5559978490658509ca41516706640fe3f3
Noah Gershenson <noah.gershenson@powerhrg.com>
2026-02-26 17:11:59 -0500

    Add event listeners for nav link formatting

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const highlightLink = (e) => {
+  console.log(e);
+  e.target.classList.add("selected");
+};
+
+const unHighlightLink = (e) => {
+  console.log(e);
+  e.target.classList.remove("selected");
+};
+
+const addLinkHoverListeners = () => {
+  const aboutUsLink = document.querySelector("#about-us-link");
+  const mediaContentLink = document.querySelector("#media-content-link");
+  const contactLink = document.querySelector("#contact-link");
+
+  aboutUsLink.addEventListener("mouseenter", highlightLink);
+  mediaContentLink.addEventListener("mouseenter", highlightLink);
+  contactLink.addEventListener("mouseenter", highlightLink);
+
+  aboutUsLink.addEventListener("mouseleave", unHighlightLink);
+  mediaContentLink.addEventListener("mouseleave", unHighlightLink);
+  contactLink.addEventListener("mouseleave", unHighlightLink);
+};
+
+const addListeners = () => {
+  addLinkHoverListeners();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  addListeners();
+});
